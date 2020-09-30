954598d2f199c374749fef7bd91fcb03e4e35181
Raznan <muhammadraznan@satpaminternet.com>
2020-09-30 17:02:40 +0700

    added r and simplified r candidate

--- main.js
+++ main.js
@@ -2,121 +2,183 @@ const scanf = require("scanf");
 const bigInt = require("big-integer")
 const process = require('process');
 
+//************************************* Function ************************************* //
+//**
+//*
+//
+let loadTime = 0;
+
 function RSA(p,q,e) {
   this.p = p;
   this.q = q;
   this.e = e;
-  temp1 = bigInt(p).minus(1)
-  temp2 = bigInt(q).minus(1)
+  this.n = bigInt(p).multiply(q);
+  let temp1 = bigInt(p).minus(1)
+  let temp2 = bigInt(q).minus(1)
   this.phi = temp1.multiply(temp2);
 }
 
 RSA.encrypt = (plaintext, key) => plaintext.modPow(key.e,key.n);
 RSA.decrypt = (ciphertext, key) =>  ciphertext.modPow(key.d,key.n);
-RSA.generateCandidate = (phi,e) => {
-  let d = [];
-  for (let k=1; d.length < 1; k++) {
-    let temp = phi.multiply(k).plus(1)
-    if(temp.isDivisibleBy(e)){
-      let temp2 = temp.divide(e);
-      d.push(temp2.toString());
-    }
+RSA.validate = (ciphertext,plaintext,key) => {
+  let msg = [];
+  loadTime = 0;
+  // Check whether the decrypted text match with the plaintext
+  for(let i = 0; i < ciphertext.length; i++) {
+    loadTime++;
+    msg.push(RSA.decrypt(bigInt(ciphertext[i]),key));
+    if(msg[i].toString() !== plaintext[i]) return false
   }
 
-  if (d.length > 0) {
-    for(let i=1; d.length < 5; i++) {
-      let temp = bigInt(d[i-1]).plus(phi);
-      d.push(temp.toString());
-    }
-  }
-  return d;
+  return true
 }
 
-RSA.prototype.generateKeys = function() {
-  let n = bigInt(this.p).multiply(this.q);
+RSA.generateCandidate = function (phiArray,{e:e,n:n} = {e: 0, n:0},ciphertext,plaintext) {
+  let d = {};
+  let test = {};
+  let decryptable = {};
+  loadTime = 0;
+  for(let i = 0; i < phiArray.length; i++){
+    let phi = bigInt(phiArray[i]);
+    let propertyPhi = phi.toString();
+    d[propertyPhi] = []
+    test[propertyPhi] = []
+    decryptable[propertyPhi] = []
+
+    for (let k=1; d[propertyPhi].length < 1; k++) { //((phi*k)+1)/e
+      let temp = phi.multiply(k).plus(1)
+      if(temp.isDivisibleBy(e)){
+        loadTime++;
+
+        let privateKey = {d:temp.divide(e),n};
+        if(RSA.validate(ciphertext,plaintext,privateKey)) d[propertyPhi].push(privateKey.d.toString());
+      }
+    }
+
+    if (d[propertyPhi].length > 0) {
+      if(i > 0){ //only for non true description
+        let tempD = bigInt(d[propertyPhi][0]); // Set first d to temp
+        if(!bigInt(d[propertyPhi][0]).isPrime()) d[propertyPhi].pop() // if not prime pop the first element
+        while(d[propertyPhi].length < 5) {
+          tempD = tempD.plus(phi); // d+phi*n
+          if(RSA.validate(ciphertext,plaintext,{d:tempD,n}) && tempD.isOdd()) {
+            loadTime++;
+            d[propertyPhi].push(tempD.toString()) //is it odd and can decrypt
+          }
+        }
+      } else {
+        for(let ii=0; d[propertyPhi].length < 5; ii++) { //d+r*n
+          let tempD = bigInt(d[propertyPhi][ii]).plus(phi); //d+phi*n
+          d[propertyPhi].push(tempD.toString());
+        }
+      }
+
+    }
+
+    //condition check
+    for(let ii=0; ii < d[propertyPhi].length; ii++) { //d+r*n
+      test[propertyPhi].push(bigInt(d[propertyPhi][ii]).isPrime())
+      decryptable[propertyPhi].push(RSA.validate(ciphertext,plaintext,{d:d[propertyPhi][ii],n}))
+    }
+  }
+
+  return [d,test,decryptable];
+}
+RSA.prototype.getAllPhi = function() { 
+  let phi = this.phi;
+  let n = this.n;
+  let phiArray = [];
+  let r = phi;
+  for(
+    let y = 0,temp = bigInt(2).pow(y); 
+    phiArray.length < 5 && r.isEven();
+    y++,temp = bigInt(2).pow(y),r = phi.divide(temp)
+  ) {
+    if(phi.isDivisibleBy(temp))
+      phiArray.push(r.toString());
+  }
+  phiArray.push(r.toString()); //last push since its not divisible anymore
+
+  return phiArray;
+}
+
+RSA.prototype.generatePublicKey = function() {
+  let n = this.n;
   let e = this.e;
-
-  let d = RSA.generateCandidate(this.phi,e);
-  /*
-  for (let k=1; d.length < 1; k++) {
-    let temp = this.phi.multiply(k).plus(1)
-    if(temp.isDivisibleBy(e)){
-      let temp2 = temp.divide(e);
-      d.push(temp2.toString());
-    }
-  }
-
-  if (d.length > 0) {
-    for(let i=1; d.length < 5; i++) {
-      let temp = bigInt(d[i-1]).plus(this.phi);
-      d.push(temp.toString());
-    }
-  }
-  */
-
   let publicKey = {e, n};
-  let candidate = d;
-
-  return [publicKey, candidate];
+  return publicKey;
 }
 
+//************************************* Start Here ************************************* //
+//**
+//*
+//
+async function main() {
+  process.stdout.write("msg: ");
+  let msg = scanf("%s");
+  let p = 0, q = 0, e = 0;
 
-process.stdout.write("msg: ");
-let msg = scanf("%s");
-let p = 0, q = 0, e = 0;
-while(1) {
-  if(!bigInt(p).isPrime()) {
-    process.stdout.write("input p [must be prime]: ");
-    p = scanf("%s");
-  } else if(!bigInt(q).isPrime()) {
-    process.stdout.write("input q [must be prime]: ");
-    q = scanf("%s");
+  // Filter for all input to only be prime
+  while(1) {
+    if(!bigInt(p).isPrime()) {
+      process.stdout.write("input p [must be prime]: ");
+      p = scanf("%s");
+    } else if(!bigInt(q).isPrime()) {
+      process.stdout.write("input q [must be prime]: ");
+      q = scanf("%s");
+    }
+    else if(!bigInt(e).isPrime()) {
+      process.stdout.write("input e [must be prime]: ");
+      e = scanf("%s");
+    } else break
   }
-  else if(!bigInt(e).isPrime()) {
-    process.stdout.write("input e [must be prime]: ");
-    e = scanf("%s");
-  } else break
+
+  let myRSA = new RSA(p,q,e);
+  console.log("Generating Phi")
+  let phiArray = myRSA.getAllPhi();
+  console.log("Generating Public key")
+  let publicKey = myRSA.generatePublicKey();
+  let privateKey = {}; 
+
+  let charCode = []; //The uncode version of each char from msg
+  let cipherText= []; //used to store calculation
+  let __cipherText = []; //used for output 
+
+  for(let i=0; i < msg.length; i++) {
+    charCode.push(msg.charCodeAt(i).toString());
+    let temp = RSA.encrypt(bigInt(charCode[i]),publicKey)
+    cipherText.push(temp);
+    __cipherText.push(temp.toString())
+  }
+  console.log("Generating Candidate for private key")
+  await let [candidate, isPrime,decryptable] = RSA.generateCandidate(phiArray,publicKey,cipherText,charCode);
+
+  console.log("msg in UTF-16: ", charCode);
+  console.log("cipher text; ", __cipherText);
+
+  privateKey.n = publicKey.n;
+  console.log("R original: ", [phiArray[0]]);
+  console.log("R simplified: ", phiArray.slice(1))
+  phiArray.pop(0);
+  console.log("Candidate: ", candidate);
+  console.log("Is prime: ", isPrime)
+  console.log("Can decrypt: ", decryptable)
+  if(candidate.length < 1) 
+    console.log("No candidate");
+  else {
+    process.stdout.write("Select Candidate for d: ");
+    let d = scanf("%s");
+    privateKey.d = bigInt(d);
+
+    let plainText = [];
+    for(let i=0; i < cipherText.length; i++) 
+      plainText.push(String.fromCharCode(RSA.decrypt(cipherText[i],privateKey) + ""));
+    console.log("Decryption: ",plainText.toString().replace(/,/g,""));
+  }
 }
 
-let myRSA = new RSA(p,q,e);
-let [publicKey, candidate] = myRSA.generateKeys();
-let privateKey = {};
+main();
 
-/* integer only
-let privateKey = {d: bigInt(candidate[0]), n: publicKey.n};
 
-let ciphertext = RSA.encrypt(msg,publicKey);
-console.log("p * q:", publicKey.n.toString());
-console.log("candidate:", candidate);
-console.log("ciphertext:", ciphertext)
-console.log("plaintext:", RSA.decrypt(ciphertext,privateKey).toString())
-*/
 
-let charCode = [];
-let cipherText= [];
-let __cipherText = [];
-
-for(let i=0; i < msg.length; i++) {
-  charCode.push(msg.charCodeAt(i));
-  let temp = RSA.encrypt(bigInt(charCode[i]),publicKey)
-  cipherText.push(temp);
-  __cipherText.push(temp.toString())
-}
-console.log("msg in UTF-16: ", charCode);
-console.log("cipher text; ", __cipherText);
-
-privateKey.n = publicKey.n;
-console.log("Candidate: ", candidate);
-if(candidate.length < 1) 
-  console.log("No candidate");
-else {
-  process.stdout.write("Select Candidate for d: ");
-  let d = scanf("%s");
-  privateKey.d = bigInt(d);
-
-  let plainText = [];
-  for(let i=0; i < cipherText.length; i++) 
-    plainText.push(String.fromCharCode(RSA.decrypt(cipherText[i],privateKey) + ""));
-  console.log("Decryption: ",plainText.toString().replace(/,/g,""));
-}
 
