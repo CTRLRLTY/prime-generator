d8ceedf474bbe460a720c7f118823ef496c9e1f0
Raznan <muhammadraznan@satpaminternet.com>
2020-10-04 10:15:54 +0700

    added incremental table

--- src/app.js
+++ src/app.js
@@ -2,9 +2,15 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import {
   Table,
-  Button
+  Button,
+  Modal,
+  ModalBody,
+  Pagination,
+  PaginationItem,
+  PaginationLink
 } from 'reactstrap';
 import {generatePrime} from './rsa'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //This function does not check if the parameter is prime, it is assume its prime
 function IsSelectedPrime(prime) {
@@ -15,7 +21,10 @@ function IsSelectedPrime(prime) {
 
 function PrimeTable(props) {
   const [value, setValue] = useState('');
+  const [modal, setModal] = useState(false);
   const [tableRows, setTableRows] = useState(null);
+  const [max, setMax] = useState(1000);
+  const [min, setMin] = useState(0);
 
   function generateTableRow(value) {
     const primeArray = generatePrime(value);
@@ -28,7 +37,9 @@ function PrimeTable(props) {
       if(!(i%10)) rowMap.push(i);
     console.log("done mapping rows");
 
-    const _tableRows = rowMap.map(row => {
+    const tempRowMap = rowMap.slice(min/10, max/10);
+    console.log(tempRowMap)
+    const _tableRows = tempRowMap.map(row => {
       const primeRows = primeMap.filter(prime => prime < row && prime > row-10);
       const primetd = primeRows.map(prime => (
         <td key={prime}>{prime}</td>
@@ -43,22 +54,51 @@ function PrimeTable(props) {
     return _tableRows;
   }
 
+
   const handleChange = event => {
     setValue(event.target.value);
   }
 
   const handleClick = () => {
-    if(value > 100000)
-      alert("You can't generate that high: (only < 100000)");
-    else
-      setTableRows(generateTableRow(value))
+    setMin(0);
+    setMax(1000);
+    setTableRows(generateTableRow(value))
+    setModal(true)
   }
-  let _Table;
-  if(tableRows)
-    _Table = '';
-  else
-    _Table = (
-      <Table striped>
+
+  const handleToggle = () => {
+    setModal(!modal);
+  }
+
+  const paginationNext = event => {
+    console.log(min,max)
+    setMin(max);
+    setMax(max+1000);
+    console.log(min,max)
+  }
+  const paginationPrev = () => {
+    console.log(min,max)
+    setMin(max-2000);
+    setMax(max-1000);
+    console.log(min,max)
+  }
+
+  useEffect(() =>{
+    setTableRows(generateTableRow(value))
+  }, [min,max])
+
+  return(
+    <div>
+      <label>
+      generate prime:<br/>
+      <input type="text" name="prime_num" value={value} onChange={handleChange}/>
+      </label>
+      <br/>
+      <Button onClick={handleClick}>Generate</Button>
+      <Button onClick={handleToggle}>Hide Table</Button>
+      <Modal isOpen={modal} toggle={handleToggle}>
+        <ModalBody>
+          <Table bordered>
             <thead>
               <tr>
                 <th>#</th>
@@ -71,18 +111,23 @@ function PrimeTable(props) {
             <tbody>
               {tableRows}
             </tbody>
-      </Table>
-    )
-
-  return(
-    <div>
-      <label>
-      generate prime:<br/>
-      <input type="text" name="prime_num" value={value} onChange={handleChange}/><br/>
-      </label>
-
-      <Button onClick={handleClick}>Generate</Button>
-      {_Table}
+          </Table>
+          <Pagination size="sm">
+            <PaginationItem>
+              <PaginationLink first/>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink previous onClick={paginationPrev}/>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink next onClick={paginationNext}/>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink last/>
+            </PaginationItem>
+          </Pagination>
+        </ModalBody>
+      </Modal>
     </div>
   )
 }
